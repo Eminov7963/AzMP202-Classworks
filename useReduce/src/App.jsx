2e9e53529d46b740abe09ccb46862bfc76c4bd18
ilgarguliyevv <s.aliyev1214@gmail.com>
2024-12-25 19:32:18 +0400

    aded{

---
+++ useReduce/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ValidationSchemaExample from './components/Counter'
+import Basic from './components/Counter'
+
+function App() {
+
+  return (
+    <>
+      <ValidationSchemaExample/>
+      
+    </>
+  )
+}
+
+export default App
